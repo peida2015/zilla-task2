66dce916b33ed390b9c6761537fc479a64adf9ae
PeiDa Kuo <guo.peida@gmail.com>
2016-12-24 09:08:39 -0800

    init-add init configs for babel webpack and gitignore

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+ module.exports = {
+     entry: './src/index.js',
+     output: {
+         path: './bin',
+         filename: 'bundle.js'
+     },
+     module: {
+         loaders: [{
+             test: /\.js$/,
+             exclude: /node_modules/,
+             loader: 'babel-loader'
+         }]
+     }
+    //  ,
+    //  plugins: [
+    //     new webpack.optimize.UglifyJsPlugin({
+    //         compress: {
+    //             warnings: false,
+    //         },
+    //         output: {
+    //             comments: false,
+    //         },
+    //     }),
+    // ]
+ };
