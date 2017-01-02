4f3dd34061cb48996d0749cc8b61f486c9ceca55
PeiDa Kuo <guo.peida@gmail.com>
2017-01-01 16:33:04 -0800

    add d3-shape for pie graphs

--- src/d3.js
+++ src/d3.js
@@ -1,16 +1,24 @@
 import { select, selectAll } from 'd3-selection';
-import * as shape from 'd3-shape';
+import { arc, pie } from 'd3-shape';
 import { csv } from 'd3-request';
 import * as collection from 'd3-collection';
-import * as axis from 'd3-axis';
-import * as array from 'd3-array';
+import { schemeAccent } from 'd3-scale-chromatic';
+import { scaleLinear, scaleOrdinal } from 'd3-scale';
+import { axisBottom, axisLeft } from 'd3-axis';
+import { min, max } from 'd3-array';
 
 export default {
   select: select,
   selectAll: selectAll,
   csv: csv,
-  shape: shape,
+  arc: arc,
+  pie: pie,
   collection: collection,
-  axis: axis,
-  array: array
+  scaleLinear: scaleLinear,
+  scaleOrdinal: scaleOrdinal,
+  axisBottom: axisBottom,
+  axisLeft: axisLeft,
+  min: min,
+  max: max,
+  schemeAccent: schemeAccent
 }
